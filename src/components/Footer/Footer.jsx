7de51348007f12dcad8a052ea4d507082e551644
Joshua Comeau <joshwcomeau@gmail.com>
2024-12-13 16:23:24 -0500

    Exercise 5 solution

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Twitter, Facebook } from 'react-feather';
 import styled from 'styled-components';
-import MaxWidthWrapper from '../MaxWidthWrapper';
 
+import { QUERIES } from '../../constants';
+
+import MaxWidthWrapper from '../MaxWidthWrapper';
 import VisuallyHidden from '../VisuallyHidden';
 
 const Footer = () => {
@@ -144,6 +146,16 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    justify-content: center;
+    gap: 48px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: flex-end;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +182,13 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    flex-direction: revert;
+    text-align: left;
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -177,6 +196,10 @@ const MainNavHeading = styled.h2`
   font-weight: var(--font-weight-bold);
   color: var(--color-gray-300);
   margin-bottom: 8px;
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: flex-start;
+  }
 `;
 
 const MainNavList = styled.ul`
